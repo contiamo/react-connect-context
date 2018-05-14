f4ee44ec4f8d7e82a8247695421c6ebc4e992e91
Fabien BERNARD <fabien0102@hotmail.com>
2018-05-14 17:51:50 +0200

    Fix types

--- index.tsx
+++ index.tsx
@@ -6,31 +6,33 @@ function defaultMergeFn<C, P>(context: C, props: P): C & P {
   return Object.assign({}, context, props);
 }
 
-type Merge<T, U> = U & (T extends U ? Partial<T> : T);
+type Merge<L, R> = 
+  /** Mandatory properties */
+  Pick<L, Exclude<keyof L, keyof R>>
+  /** Optional properties */
+  & Partial<R>
 
 export function connectContext<C, P>(
   ContextConsumer: React.Consumer<C>,
   mergeContextWithProps: MergeContextWithProps<C, P> = defaultMergeFn
-): (Component: React.SFC<P & C>) => React.SFC<P> {
-  // Here we have a real challenge, the component props type should have
-  // all the original component props with an overide by the context
-  // so something like SFC<C extends Partial<P>>
-  return function(Component: React.SFC<Merge<C, P>>): React.SFC<P> {
-    return function(props: P): JSX.Element {
-      // if (!ContextConsumer.currentValue instanceof Object) {
-      //   throw new Error(
-      //     `react-connect-context: the current value of the given context identifier is _not_ an object,
-      //   and therefore cannot be passed as props to ${
-      //     Component.name
-      //   }. Please check the value in the context
-      //   and try again.
+) {
+  return function(Component: React.SFC<P>) {
+    return function(props: Merge<P, C>) {
+      // TODO remove `as any` when is available in the react typing definition
+      if (!(ContextConsumer as any).currentValue as any instanceof Object) {
+        throw new Error(
+          `react-connect-context: the current value of the given context identifier is _not_ an object,
+        and therefore cannot be passed as props to ${
+          Component.name
+        }. Please check the value in the context
+        and try again.
 
-      //   More info: https://github.com/Contiamo/react-connect-context#gotchas`
-      //   );
-      // }
+        More info: https://github.com/Contiamo/react-connect-context#gotchas`
+        );
+      }
       return (
         <ContextConsumer>
-          {context => <Component {...mergeContextWithProps(context, props)} />}
+          {context => <Component {...mergeContextWithProps(context, props as P)} />}
         </ContextConsumer>
       );
     };
